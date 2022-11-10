c0b127066d73a33f8162b6f4913d91c03a3b31c8
jeremie.tisserand <jeremie.tisserand@kuehne-nagel.com>
2022-11-10 22:37:29 +0100

    Remove unused import

--- src/main/java/com/zenika/demo/podcast/in/Scheduler.java
+++ src/main/java/com/zenika/demo/podcast/in/Scheduler.java
@@ -1,6 +1,5 @@
 package com.zenika.demo.podcast.in;
 
-import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
